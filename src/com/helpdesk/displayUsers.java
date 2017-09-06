2c78a8141108992e4af98f4d4199590d87ef2ffb
juniormbhele <juniormbhele@users.noreply.github.com>
2017-09-06 19:16:17 +0200

    Changes on this day 07 September 20117
    Still facing an issue with Displayig a result set to a table for displaying Users.

--- src/com/helpdesk/displayUsers.java
+++ src/com/helpdesk/displayUsers.java
@@ -39,67 +39,6 @@ public class displayUsers extends HttpServlet {
     }
 
 
-    public static List<UserDto> displayUsers(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        List<UserDto> users = null;
-        try {
-            Connection con = Database.getConnection();
-
-            if (con != null) {
-                PreparedStatement ps = con.prepareStatement("SELECT * FROM SYSTEM.USERS");
-                ResultSet rs = ps.executeQuery();
-
-                ResultSetMetaData md = rs.getMetaData();
-                int columnCount = md.getColumnCount();
-
-
-                while (rs.next()) {
-
-                    UserDto user = new UserDto();
-
-                    for (int i = 0; i <= columnCount; i++)
-                    {
-                        user.setUsername(rs.getString("USERNAME"));
-                        user.setFirstName(rs.getString("FIRSTNAME"));
-                        user.setLastName(rs.getString("LASTNAME"));
-                        user.setEmail(rs.getString("EMAIL"));
-                        user.setCompany(rs.getString("COMPANY"));
-                        user.setAddress(rs.getString("ADDRESS"));
-                        user.setCity(rs.getString("CITY"));
-                        user.setCountry(rs.getString("COUNTRY"));
-                        user.setPortalCode(rs.getString("PORTALCODE"));
-                        user.setAbout(rs.getString("ABOUT"));
-                        user.setRole(rs.getString("ROLE"));
-                        user.setReg_date(rs.getString("REG_DATE"));
-
-
-                    }
-                    users.add(user);
-                }
-
-//
-//                }
-//                else
-//                {
-//                    System.out.println("Results set is empty");
-//                }
-
-                rs.close();
-                con.close();
-
-
-            } else {
-                System.out.println("Not connection for the database");
-            }
-
-
-        } catch (Exception ex) {
-            System.out.println(ex);
-
-        }
-
-        return users;
-    }
 
     public List<UserDto> addUsers(HttpServletRequest request, HttpServletResponse response) throws ClassNotFoundException, SQLException
     {
@@ -108,7 +47,7 @@ public class displayUsers extends HttpServlet {
         Connection con = Database.getConnection();
         PreparedStatement ps = con.prepareStatement("SELECT * FROM SYSTEM.USERS");
         ResultSet rs = ps.executeQuery();
-
+        rs.beforeFirst();
         while(rs.next())
         {
             UserDto user = new UserDto();
